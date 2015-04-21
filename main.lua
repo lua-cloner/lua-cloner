152031b4d3bbe91e4dc8f7051b6b14a4e16d4a13
TurtleHunter <turtlehunter@users.noreply.github.com>
2015-04-20 23:01:17 -0300

    Added current code

---
+++ main.lua
@@ -0,0 +1,20 @@
+require'lfs'
+
+disks={}
+useDisk = ""
+
+for file in lfs.dir('/dev/') do 
+	if lfs.attributes(file).mode=='block device' then 
+		if not file:find'loop' or file:find'dvd' or file:find'cdrom' then
+			table.insert(disks,file)
+		end 
+	end 
+end
+
+for i=1, #disks do
+	print(i.."- "+disks[i])
+end
+
+i = read();
+useDisk = disks[i]
+disks = null --Free memory
