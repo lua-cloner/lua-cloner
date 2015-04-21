b5f84f93f47c6bfdbcded9bdaab87e6f17873999
Uriel Salischiker <urielsalisgaming@gmail.com>
2015-04-20 23:19:53 -0300

    Added code

--- LUA-Clone/src/main.lua
+++ LUA-Clone/src/main.lua
@@ -1,4 +1,24 @@
-local function main()
+require'lfs'
 
+disks={}
+useDisk = ""
+
+local function main()
+  for file in lfs.dir('/dev/') do 
+    if lfs.attributes(file).mode=='block device' then 
+      if not file:find'loop' or file:find'dvd' or file:find'cdrom' then
+        table.insert(disks,file)
+      end 
+    end 
+  end
+  
+  for i=1, #disks do
+    print(i.."- "+disks[i])
+  end
+  
+  i = read();
+  useDisk = disks[i]
+  disks = null --Free memory
 end
+
 main()
