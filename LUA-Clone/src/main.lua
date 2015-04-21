3de74a55a68cce3a6f9ab11339713d2867fb874c
Uriel Salischiker <urielsalisgaming@gmail.com>
2015-04-21 00:07:20 -0300

    Add a menu, todos

--- LUA-Clone/src/main.lua
+++ LUA-Clone/src/main.lua
@@ -1,26 +1,23 @@
 require'lfs'
 local class = require 'middleclass'
 
-disks={}
-useDisk = ""
+screen = {function clear() end, function setPos(x, y) end, function getPos() end, function read() end}
 
 local function main()
   init()
-  for file in lfs.dir('/dev/') do 
-    if lfs.attributes(file).mode=='block device' then 
-      if not file:find'loop' or file:find'dvd' or file:find'cdrom' then
-        table.insert(disks,file)
-      end 
-    end 
+
+  result, i = Menu:showMain()
+  
+  if i==1 then
+    Menu:showCreateImage(disks)  --todo
+  else if i==2 then
+    Menu:showCreateMultiImage()  --todo create
+  else if i==3 then
+    Menu:showIncrementImage()  --todo create
+  else if i==4 then
+    Menu:showRestoreImage()  --todo create
   end
   
-  for i=1, #disks do
-    print(i.."- "+disks[i])
-  end
-  
-  i = read();
-  useDisk = disks[i]
-  disks = null --Free memory
 end
 
 init()
@@ -33,5 +30,57 @@ end
 
 --Fuctions
 function init() 
-
+  Menu = class("Menu")
 end
+
+function Menu:initialize() 
+  --unused
+end
+
+function Menu:showMain()
+
+  screen.clear();
+  screen.setPos(0,0)
+  print("lua-clone V0.0.1-a") --major.minor.patch-letter  major = Breaks saves/images   minor = Adds feature/stable  patch = bugfix  letter= a=alpha, b=beta, r=release, t=testing, s=stable 
+  print("---------------")
+  print()
+  
+  print("1- Create Image")
+  print("2- Create MultiImage")
+  print("3- Increment Image")
+  print("4- Restore Image")
+  while true do
+    i = tonumber(io.read())
+    if i>0 an i<5 then
+      return true, i
+    end
+  end
+end
+function Menu:showCreateImage(disks)
+  local useDisk
+  
+  local disks={}
+  for file in lfs.dir('/dev/') do 
+    if lfs.attributes(file).mode=='block device' then 
+      if not file:find'loop' or file:find'dvd' or file:find'cdrom' then
+        table.insert(disks,file)
+      end 
+    end 
+  end
+  
+  screen.clear();
+  screen.setPos(0,0)
+  print("Choose a option")
+  for i=1, #disks do
+    print("  "..i.."- "+disks[i])
+  end
+  while true do
+    i = tonumber(io.read())
+    if i>0 and i=<#disks then
+      useDisk = disks[i]
+      break
+    end
+  end
+  return true
+end
+
