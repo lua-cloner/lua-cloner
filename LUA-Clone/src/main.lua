ce4c8ab4a740226b81437df9f18c19e00f241cd9
Uriel Salischiker <urielsalisgaming@gmail.com>
2015-04-20 23:34:08 -0300

    Add error checking

--- LUA-Clone/src/main.lua
+++ LUA-Clone/src/main.lua
@@ -5,6 +5,7 @@ disks={}
 useDisk = ""
 
 local function main()
+  init()
   for file in lfs.dir('/dev/') do 
     if lfs.attributes(file).mode=='block device' then 
       if not file:find'loop' or file:find'dvd' or file:find'cdrom' then
@@ -22,6 +23,15 @@ local function main()
   disks = null --Free memory
 end
 
-main()
+init()
+ok, err = pcall(main)
+if not ok then
+  print("ERROR: "..err)
+  --TODO shutdown hook
+end
 
+
+--Fuctions
 function init() 
+
+end
